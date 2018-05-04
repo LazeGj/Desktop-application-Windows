729b0567be47b843070a57061ae1341ae7596f99
Laze <gorgiev.laze@yahoo.com>
2018-05-04 16:57:44 +0200

    Pacman unfinished game

---
+++ Pacman/Pacman/Form1.cs
@@ -0,0 +1,104 @@
+﻿using Pacman.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pacman
+{
+    public partial class Form1 : Form
+    {
+        private Pacman game;
+        
+        static readonly int TIMER_INTERVAL = 250;
+        static readonly int WORLD_WIDTH = 15;
+        static readonly int WORLD_HEIGHT = 10;
+        Timer timer;
+        Image foodImage;
+        bool[][] foodWorld;
+        Point[][] foodLocation;
+        
+        
+        public Form1()
+        {
+            this.DoubleBuffered = true;
+            game = new Pacman();
+            timer = new Timer();
+            timer.Enabled = true;
+            timer.Start();
+            foodImage = Resources.GameOrange;
+       
+            InitializeComponent();
+
+            foodWorld = new bool[15][];
+            for(int i=0;i<foodWorld.Length;++i)
+            {
+                foodWorld[i] = new bool[10];
+            }
+
+            for (int i = 0; i < foodWorld.Length; ++i)
+            {
+                for (int j = 0; j < foodWorld[0].Length; ++j)
+                    foodWorld[i][j] = true;
+            }
+
+            foodLocation = new Point[15][];
+            for (int i = 0; i < foodWorld.Length; ++i)
+            {
+                foodLocation[i] = new Point[10];
+            }
+
+            for (int i = 0; i < foodWorld.Length; ++i)
+            {
+                for (int j = 0; j < foodWorld[0].Length; j++)
+                        foodLocation[i][j] = new Point(i*40+10,j*40+10);
+            }
+        }
+
+
+        private void drawOranges(Graphics g)
+        {
+            for(int i=0;i<foodWorld.Length;++i)
+            {
+                for(int j=0;j<foodWorld[0].Length;++j)
+                {
+                    if(i==7 && j==5)
+                    {
+                        continue;
+                    }
+                    else if(foodWorld[i][j])
+                    {
+                        //MessageBox.Show("dsads");
+                        g.DrawImage(foodImage,foodLocation[i][j].X-20, foodLocation[i][j].Y-20, 2*20,2*20);
+                    }
+                }
+            }
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            drawOranges(e.Graphics);
+            game.Draw(e.Graphics);
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            game.Move(20,20,this.Width,this.Height);
+            for(int i=0;i<foodLocation.Length;++i)
+            {
+                for(int j=0;j<foodLocation[0].Length;++j)
+                {
+                    if (game.eat(foodLocation[i][j]))
+                        foodWorld[i][j] = false;
+                }
+            }
+
+            Invalidate(true);
+        }
+    }
+}
---
+++ Pacman/Pacman/Pacman.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman
+{
+    public enum Direction
+    {
+        up,
+        down,
+        left,
+        right
+    }
+    public class Pacman
+    {
+        public Point Center { get; set; }
+
+        public Direction Direction { get; set; }
+
+        public int Radius { get; set; }
+        public int Speed { get; set; }
+
+        public bool OpenMouth { get; set; }
+
+        public Brush MyBrush { get; set; }
+
+        public Pacman()
+        {
+            Center = new Point(20*7,5*20);
+            Direction = Direction.right;
+            MyBrush = new SolidBrush(Color.Yellow);
+            OpenMouth = true;
+            Radius = 20;
+            Speed = Radius;
+        }
+
+        public void ChangeDirecton(Direction direction)
+        {
+            Direction = direction;
+        }
+
+
+        private bool checkValidate(Point center, int width,int height,int startX,int startY)
+        {
+            if (center.X < startX)
+                return false;
+            if (center.Y < startY)
+                return false;
+            if (center.X > width)
+                return false;
+            if (center.Y > height)
+                return false;
+            return true;
+        }
+        public void Move(int startX,int startY,int Widht,int Height)
+        {
+            OpenMouth = !OpenMouth;
+
+            if (Direction == Direction.down)
+            {
+                Center = new Point(Center.X, Center.Y + 1*20);
+                if (!checkValidate(Center, Widht, Height, startX, startY)) ;
+                {
+                    Center = new Point(Center.X, 20);
+                }
+            }
+
+            else if (Direction == Direction.up)
+            {
+                Center = new Point(Center.X, Center.Y - 1*20);
+
+                if (!checkValidate(Center, Widht, Height, startX, startY)) ;
+                {
+
+                    Center = new Point(Center.X, Height - 20);
+                }
+            }
+
+            else if (Direction == Direction.right)
+            {
+                Center = new Point(Center.X + 20, Center.Y);
+                if (!checkValidate(Center, Widht, Height, startX, startY)) ;
+                {
+
+                    Center = new Point(20, Center.Y);
+                }
+
+            }
+
+            else
+            {
+                Center = new Point(Center.X - 20, Center.Y);
+                if (!checkValidate(Center, Widht, Height, startX, startY)) ;
+                {
+                    
+                    Center = new Point(Widht-20, Center.Y);
+                }
+            }
+
+
+
+        }
+
+        public bool eat(Point orangeCenter)
+        {
+            var diff = Math.Abs((Center.X - orangeCenter.Y) + (Center.Y - orangeCenter.Y));
+
+            if (diff < 20)
+                return true;
+            return false;
+        }
+
+        public void  Draw(Graphics g)
+        {
+            if(OpenMouth)
+            {
+                g.FillPie(this.MyBrush, new Rectangle(Center.X - Radius, Center.Y - Radius, 2 * Radius, 2 * Radius), 45, 315);
+                return;
+            }
+
+            g.FillEllipse(this.MyBrush, Center.X - Radius, Center.Y - Radius, 2 * Radius, 2 * Radius);
+
+        }
+    }
+}
